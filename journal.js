e7a5d02d9f0643ce7cde7362a28dd5502a228eb9
Natacha <natacha.har@onja.org>
2020-06-12 11:39:17 +0300

    showing prompt

--- journal.js
+++ journal.js
@@ -1,1 +1,25 @@
 console.log('it works');
+let lists = [ {
+        title : "Sunday 1, 2020",
+        content : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae aspernatur earum culpa quae veritatis ipsum sequi beatae, perferendis corporis architecto quasi debitis fuga accusamus in eum veniam animi voluptates id!"
+    },
+    {
+        title : "Lucky day",
+        content : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae aspernatur earum culpa quae veritatis ipsum sequi beatae, perferendis corporis architecto quasi debitis fuga accusamus in eum veniam animi voluptates id!"
+    },
+    {
+        title : "Memoring day",
+        content : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae aspernatur earum culpa quae veritatis ipsum sequi beatae, perferendis corporis architecto quasi debitis fuga accusamus in eum veniam animi voluptates id!"
+    },
+    {
+        title : "New year, 2020",
+        content : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae aspernatur earum culpa quae veritatis ipsum sequi beatae, perferendis corporis architecto quasi debitis fuga accusamus in eum veniam animi voluptates id!"
+    }
+];
+
+
+
+let journalEntry = prompt("Welcome to my onja journal! \n Choose(1) for listing all the entries. \n Choose (2) for addind a new entries. \n Choose (3) to quit.");
+
+
+
