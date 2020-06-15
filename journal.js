17c0f0b3078b2c7bf022fde2efba2d30bff89a7c
Natacha <natacha.har@onja.org>
2020-06-15 08:41:12 +0300

    show the first entry

--- journal.js
+++ journal.js
@@ -1,5 +1,5 @@
 console.log('it works');
-let lists = [ {
+const journal = [ {
         title : "Sunday 1, 2020",
         content : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae aspernatur earum culpa quae veritatis ipsum sequi beatae, perferendis corporis architecto quasi debitis fuga accusamus in eum veniam animi voluptates id!"
     },
@@ -18,8 +18,11 @@ let lists = [ {
 ];
 
 
-
 let journalEntry = prompt("Welcome to my onja journal! \n Choose(1) for listing all the entries. \n Choose (2) for addind a new entries. \n Choose (3) to quit.");
+
+if (users = 1) {
+    alert(`title:  ${journal[0].title} \ncontent: ${journal[0].content}`);
+}
 
 
 
