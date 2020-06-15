b3562b6676a59eb92e7f73f41159e6de552869d1
Natacha <natacha.har@onja.org>
2020-06-15 11:49:47 +0300

    asked the users to enter a journal entry and an entry content

--- journal.js
+++ journal.js
@@ -18,16 +18,25 @@ const journal = [ {
 ];
 
 
-let journalEntry = Number(prompt("Welcome to my onja journal! \n Choose(1) for listing all the entries. \n Choose (2) for addind a new entry. \n Choose (3) to quit."));
+let journalEntry = Number(prompt("Welcome to my onja journal! \n Choose(1) for listing all the entries. \n Choose (2) for addind a new entries. \n Choose (3) to quit. \n Choose (4) for deleting the last entry"));
+
 
 if (journalEntry === 1) {
     alert(`title:  ${journal[0].title} \ncontent: ${journal[0].content}`);
+    alert(`title:  ${journal[1].title} \ncontent: ${journal[1].content}`);
+    alert(`title:  ${journal[2].title} \ncontent: ${journal[2].content}`);
+    alert(`title:  ${journal[3].title} \ncontent: ${journal[3].content}`);
+    }
+
+
+if (journalEntry === 2) {
+    let entryTitle = prompt("Enter your journal title");
+    let entryContent = prompt("Enter your entry content");
+        alert(`title: ${entryTitle} \ncontent: ${entryContent}`);
 }
 
 
-if (journalEntry === 2);
-alert("Add a new entry");
-entryTitle = prompt("Add a new title");
+
 
 
 
