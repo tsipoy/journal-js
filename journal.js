df5053dd31971496f3247863d93942b2f684e48a
Natacha <natacha.har@onja.org>
2020-06-15 09:18:47 +0300

    asked thr user to put their entry

--- journal.js
+++ journal.js
@@ -18,11 +18,16 @@ const journal = [ {
 ];
 
 
-let journalEntry = prompt("Welcome to my onja journal! \n Choose(1) for listing all the entries. \n Choose (2) for addind a new entries. \n Choose (3) to quit.");
+let journalEntry = Number(prompt("Welcome to my onja journal! \n Choose(1) for listing all the entries. \n Choose (2) for addind a new entry. \n Choose (3) to quit."));
 
-if (users = 1) {
+if (journalEntry === 1) {
     alert(`title:  ${journal[0].title} \ncontent: ${journal[0].content}`);
 }
+
+
+if (journalEntry === 2);
+alert("Add a new entry");
+entryTitle = prompt("Add a new title");
 
 
 
